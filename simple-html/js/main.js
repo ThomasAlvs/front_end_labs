2817604d4d6a187e1465d1f0c97a59f59550de87
ThomasAlvs <thomascouto999@gmail.com>
2025-05-21 20:34:29 -0300

    Virutal phone part 1

--- simple-html/js/main.js
+++ simple-html/js/main.js
@@ -1,2 +1,6 @@
-let nome = "Thomas";
-    alert("Salve, " + nome);
+const linkExterno = 
+    document.querySelector('a[title="Ir para o Google Maps"]');
+
+linkExterno.addEventListener('click', () => {
+    alert("Você será redirecionado a um link externo")
+})
